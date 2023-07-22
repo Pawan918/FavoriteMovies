8a6c463b63e67068607d0d7f10ef873815478a56
Pawan918 <pawanshah84489@gmail.com>
2023-07-22 20:55:29 +0530

    22/07/23

--- src/Componenets/Loader/Loader.jsx
+++ src/Componenets/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './loader.scss'
 function Loader() {
   return (
-    <div className='loader-container'> 
+    <div className='loader-container'>
       <div className='spinner'></div>
     </div>
   )
